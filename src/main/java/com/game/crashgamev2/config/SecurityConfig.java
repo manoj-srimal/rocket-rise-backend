0fffd708502afcf9590e970461f33c94d42c6772
ADMIN <srimalmanoj56@gmail.com>
2025-10-19 08:57:45 +0530

    Initial commit of backend code

--- src/main/java/com/game/crashgamev2/config/SecurityConfig.java
+++ src/main/java/com/game/crashgamev2/config/SecurityConfig.java
@@ -79,7 +79,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "https://astounding-kheer-88647a.netlify.app"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
